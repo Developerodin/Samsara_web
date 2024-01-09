960a6254bdaef249ec91f45743328fea83490f4c
Akshaypareek01 <akshay96102@gmail.com>
2024-01-09 14:31:58 +0530

    working

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import {Login} from "./Pages/Login"
 import {Articles} from "./Pages/Articles"
 import {Pricing} from "./Pages/Pricing"
 import { ConformationPage } from './Pages/ConformationPage';
+import { TrainersSignup } from './Pages/TrainersSignup';
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
     <Routes>
        
-          <Route path='/' element={<Login/>} /> 
+          <Route path='/' element={<SignUp2/>} /> 
           <Route path='/signup' exact element={ <SignUp2/>} />
+          <Route path='/trainers-signup' exact element={ <TrainersSignup/>} />
           <Route path='/login' exact element={ <Login/>} />
           <Route path='/articles' exact element={ <Articles/>} />
           <Route path='/pricing' exact element={ <Pricing/>} />
