1d29c1019d80f39256885a9426b620cee27c8147
Akshaypareek01 <akshay96102@gmail.com>
2024-07-29 11:59:13 +0530

    working

--- src/Config/BaseUrl.jsx
+++ src/Config/BaseUrl.jsx
@@ -1,3 +1,3 @@
 
-// export const Base_url= "https://samsara-backend.onrender.com/"
- export const Base_url= "http://localhost:4000/"
+export const Base_url= "https://samsara-backend.onrender.com/"
+//  export const Base_url= "http://localhost:4000/"
