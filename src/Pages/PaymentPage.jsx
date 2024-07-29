16d875747aa56f1041da47fcf13558b1b42b96e8
Akshaypareek01 <akshay96102@gmail.com>
2024-07-29 11:56:39 +0530

    working

--- src/Pages/PaymentPage.jsx
+++ src/Pages/PaymentPage.jsx
@@ -5,12 +5,7 @@ import { Base_url } from '../Config/BaseUrl';
 
 export const PaymentPage = () => {
     const { userId } = useParams();
-    const [userData, setUserData] = useState({
-        name: '',
-        email: '',
-        mobile: '',
-        address: '',
-    });
+    const [userData, setUserData] = useState(null);
 
     const getUser = async () => {
         try {
@@ -104,27 +99,99 @@ export const PaymentPage = () => {
         window.location.href = 'samsara://home'; // Adjust this to match your app's deep link
       };
 
+      const loadScript = (src) => {
+        return new Promise((resolve) => {
+            const script = document.createElement('script');
+            script.src = src;
+            script.onload = () => {
+                resolve(true);
+            };
+            script.onerror = () => {
+                resolve(false);
+            };
+            document.body.appendChild(script);
+        });
+    };
+
+    const displayRazorpay = async () => {
+        const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js');
+
+        if (!res) {
+            alert('Razorpay SDK failed to load. Are you online?');
+            return;
+        }
+
+        const result = await axios.post(`${Base_url}payment/orders`);
+
+        if (!result) {
+            alert('Server error. Are you online?');
+            return;
+        }
+
+        const { amount, id: order_id, currency } = result.data;
+
+        const options = {
+            key: 'rzp_test_u6ftkngPEG7shl', // Enter the Key ID generated from the Dashboard
+            amount: amount.toString(),
+            currency: currency,
+            name: 'Samsara Wellness',
+            description: 'Test Transaction',
+            // image: logo,
+            order_id: order_id,
+            handler: async function (response) {
+                const data = {
+                    orderCreationId: order_id,
+                    razorpayPaymentId: response.razorpay_payment_id,
+                    razorpayOrderId: response.razorpay_order_id,
+                    razorpaySignature: response.razorpay_signature,
+                };
+
+                const result = await axios.post(`${Base_url}payment/success`, data);
+
+                alert(result.data.msg);
+                handleBackToApp()
+            },
+            prefill: {
+                name: userData.name,
+                email: userData.email,
+                contact:userData.mobile,
+            },
+            notes: {
+                address:userData.address,
+            },
+            theme: {
+                color: '#61dafb',
+            },
+        };
+
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+    };
+
 
     return (
         <div style={formContainerStyle}>
             <form onSubmit={handleSubmit} style={formStyle}>
                 <div>
                     <label>Name:</label>
-                    <input type="text" value={userData.name} readOnly style={inputStyle} />
+                    <input type="text" value={userData && userData.name} readOnly style={inputStyle} />
                 </div>
                 <div>
                     <label>Email:</label>
-                    <input type="text" value={userData.email} readOnly style={inputStyle} />
+                    <input type="text" value={userData && userData.email} readOnly style={inputStyle} />
                 </div>
                 <div>
                     <label>Mobile:</label>
-                    <input type="text" value={userData.mobile} readOnly style={inputStyle} />
+                    <input type="text" value={userData && userData.mobile} readOnly style={inputStyle} />
                 </div>
                 <div>
                     <label>Address:</label>
-                    <input type="text" value={userData.address} readOnly style={inputStyle} />
+                    <input type="text" value={userData && userData.address} readOnly style={inputStyle} />
                 </div>
-                <button onClick={handleBackToApp} style={buttonStyle}>Proceed to Payment</button>
+                {
+                    userData && <button onClick={displayRazorpay} style={buttonStyle}>Proceed to Payment</button>
+                }
+                
             </form>
         </div>
     );
